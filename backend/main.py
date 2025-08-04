24e571705a5f366aa908bc5b4fba20e6f14d48c4
Roger <rogerzapatista@gmail.com>
2025-08-04 12:39:39 -0600

    .

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,9 @@
-
 from fastapi import FastAPI
 from auth.routes import router as auth_router
 from drive.routes import router as drive_router
 from certificados.routes import router as certificados_router
 from routes.contacto import router as contacto_router
+from translate.routes import router as translate_router
 import os
 from dotenv import load_dotenv
 
@@ -15,6 +15,7 @@ app.include_router(auth_router, prefix="/auth")
 app.include_router(drive_router, prefix="/drive")
 app.include_router(certificados_router)
 app.include_router(contacto_router, prefix="/api")
+app.include_router(translate_router, prefix="/translate")
 
 @app.get("/")
 def root():
